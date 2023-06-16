7989ca1e4f6df57f6219a2293c9cc792478169d6
DESKTOP-A44SAJL\신준 <gkstlswns@gmail.com>
2023-06-16 09:49:48 +0900

    shinjun06/16 0948

--- camperblic/src/main/frontend/src/pages/item/ItemAll.tsx
+++ camperblic/src/main/frontend/src/pages/item/ItemAll.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Item} from "../../types";
 import axios from "axios";
 import ItemLayOut from "./ItemLayOut";
-import TopLayOut from "./TopLayOut";
+
 
 const ItemAll = () => {
     const [items, setItems] = useState<Item[]>([]);
@@ -14,12 +14,9 @@ const ItemAll = () => {
     }, []);
 
     return (
-        <>
-            <TopLayOut>전체상품</TopLayOut>
-        <ul>
-            <ItemLayOut items={items}/>
-        </ul>
-            </>
+
+            <ItemLayOut items={items} topText="전체상품"/>
+
     );
 };
 
